0885cfbfe90f5a4b3c337716bafbc281cbc61082
ryanking-3 <ryan.king-3@selu.edu>
2026-04-24 12:06:02 -0500

    ds

--- project/routes/topics.js
+++ project/routes/topics.js
@@ -1,18 +1,18 @@
-// routes/topics.js
 const express = require('express');
 const router  = express.Router();
 const topicController   = require('../controllers/topicController');
 const messageController = require('../controllers/messageController');
 const { requireAuth } = require('../middleware/auth');
 
-router.get('/',           requireAuth, topicController.getAllTopics);
-router.get('/new',        requireAuth, topicController.getNewTopic);
-router.post('/',          requireAuth, topicController.postNewTopic);
-router.get('/:id',        requireAuth, topicController.getTopic);
+// Topic Management
+router.get('/',               requireAuth, topicController.getAllTopics);
+router.get('/new',            requireAuth, topicController.getNewTopic);
+router.post('/new',           requireAuth, topicController.postNewTopic); // Fixed path
+router.get('/:id',            requireAuth, topicController.getTopic);
 router.post('/:id/subscribe',   requireAuth, topicController.subscribe);
 router.post('/:id/unsubscribe', requireAuth, topicController.unsubscribe);
 
-// Messages within a topic
+// Messages
 router.post('/:topicId/messages',        requireAuth, messageController.postMessage);
 router.post('/messages/:id/delete',      requireAuth, messageController.deleteMessage);
 
